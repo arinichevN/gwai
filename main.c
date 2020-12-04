1c6e929e7e19a7a451ff922ecb4d555a62066664
arinichevN <arinichev_n@mail.ru>
2020-12-04 04:45:52 +0300

    c

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-
 static Serverm server;
 
 static Mutex serial_thread_list_mutex = MUTEX_INITIALIZER;
 static SerialThreadStarter serial_thread_starter;
 static SerialThreadLList serial_thread_list = LLIST_INITIALIZER;
 static ChannelList channel_list = LIST_INITIALIZER;
-
 
 #include "model/SlaveGetCommand.c"
 #include "model/SlaveSetCommand.c"
@@ -19,9 +17,6 @@ static ChannelList channel_list = LIST_INITIALIZER;
 #include "util.c"
 #include "server.c"
 #include "app.c"
-
-
-
 
 int main ( int argc, char** argv ) {
 #ifdef MODE_FULL
